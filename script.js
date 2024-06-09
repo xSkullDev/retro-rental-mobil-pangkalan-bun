0d6ba33fbf187693e3220e4241a9ca1f58a48178
xSkullDev <x.skull.dev@gmail.com>
2024-06-10 02:02:45 +0700

    update

--- script.js
+++ script.js
@@ -216,7 +216,3 @@ function adminOtomatis3() {
 
     window.open(url, '_blank');
 }
-
-document.getElementById('contactDiv').addEventListener('click', function() {
-    window.open('https://soft-footed-deputie.000webhostapp.com/index.html', '_blank');
-});

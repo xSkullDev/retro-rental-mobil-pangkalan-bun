3f78ace19eba12a450ee5b819e95ba178a7be6f1
xSkullDev <x.skull.dev@gmail.com>
2024-06-12 04:00:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,6 @@ function adminOtomatis3() {
 
 // Function Maps
 document.getElementById('lokasi').addEventListener('click', function() {
-    const address = "https://www.google.com/maps/place/Asrama+IPMK+Semarang/@-6.984326,110.4142744,17z/data=!3m1!4b1!4m6!3m5!1s0x2e708b4118d1a461:0xa3ad6786ec5a3efd!8m2!3d-6.9843313!4d110.4168493!16s%2Fg%2F11h2fmd2vq?entry=ttu";
-    const googleMaps = 'https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}';
+    const googleMaps = 'https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(https://www.google.com/maps/place/Asrama+IPMK+Semarang/@-6.984326,110.4142744,17z/data=!3m1!4b1!4m6!3m5!1s0x2e708b4118d1a461:0xa3ad6786ec5a3efd!8m2!3d-6.9843313!4d110.4168493!16s%2Fg%2F11h2fmd2vq?entry=ttu)}';
     window.open(googleMaps, '_blank');
 })
